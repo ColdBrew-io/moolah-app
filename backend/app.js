11c534b17f000fd60f30b46b20474652d79f8ee8
OskyJr <49112599+OskyJr@users.noreply.github.com>
2021-11-19 01:53:58 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 
+//TODO: finish setup via https://www.youtube.com/watch?v=ZZp0VIjTsbM (18:00)
+
 // set to use either the env.PORT macro or 3000
 const port = process.env.PORT || 3000
 
